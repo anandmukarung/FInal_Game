309dcfce5c329eff8b604d2dbc2bd6c4e3523879
jngeronimo <ajboyger@yahoo.com>
2022-04-13 15:40:59 -0700

    models

--- bin/assets/scripts/InitParams.js
+++ bin/assets/scripts/InitParams.js
@@ -7,12 +7,16 @@ var JavaPackages = new JavaImporter(
 var rumble = .001;
 var objectScale = 0.2;
 var dolphinScale = 3.0;
+var GaurdScale = 3.0;
+var JanitorScale = 3.0;
 var groundScale = 10000.0;
 var streetlightScale = 4.0;
 var buildingScale = 10.0;
 var cityScale = 5.0;
 
 with (JavaPackages){
+	var JanitorTranslate = new Vector3f(0.0, 7.0, 10.0);
+	var GaurdTranslate = new Vector3f(10.0, 7.0, 0.0);
 	var dolphinTranslate = new Vector3f(0.0, 7.0, 0.0);
 	var garbageScale = new Vector3f(10.0, 10.0, 10.0);
 	var streetlightTranslate = new Vector3f(5.0, 0.0, 0.0);
